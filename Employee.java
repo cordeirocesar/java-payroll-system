438a051739d3a929d8b99fb84734a7b0be6756e9
Cordeiro Cesar <cesarcordeiromphc@gmail.com>
2026-09-21 13:05:26 +0200

    add salary to Employee

--- Employee.java
+++ Employee.java
@@ -1,2 +1,19 @@
 public class Employee {
+
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(double salary) {
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
