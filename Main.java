31ba8e6165cdcf756799f8c85ca291f4df7c2a28
Cordeiro Cesar <cesarcordeiromphc@gmail.com>
2026-09-21 16:25:34 +0200

    instantiate employee objects in Main

--- Main.java
+++ Main.java
@@ -2,5 +2,19 @@ public class Main {
 
     static void main(String[] args) {
 
+        Employee developer = new Developer();
+        developer.setName("Francesco Totti");
+        developer.setEmail("francesco@developer.it");
+        developer.setSalary(8000);
+
+        Employee productManager = new ProductManager();
+        productManager.setName("Giulia Tofana");
+        productManager.setEmail("giuliatofana@productmanager.it");
+        productManager.setSalary(10000);
+
+        Employee devOpsEngineer = new DevOpsEngineer();
+        devOpsEngineer.setName("Andrea Pirlo");
+        devOpsEngineer.setEmail("andreapirlo@devopsengineer.it");
+        devOpsEngineer.setSalary(9000);
     }
 }
